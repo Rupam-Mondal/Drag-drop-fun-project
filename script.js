855e8fa80b70bc550d3a21e5c7de9a17f153d0a4
Rupam <rupammondal564@gmail.com>
2024-02-10 19:07:12 +0530

    mouse move event added

--- script.js
+++ script.js
@@ -1,11 +1,55 @@
+let maxz = 1;
+
+
 class Paper{
+    holdpaper = false;
+    premouseX = 0;
+    premouseY = 0;
+
+    mouseX = 0;
+    mouseY = 0;
+
+    velocityX = 0;
+    velocityY = 0;
+
+    currPaperX = 0;
+    currPaperY = 0;
+
+
     init(paper){
         paper.addEventListener('mousedown' , (e) => {
-            console.log("Selected");
+            this.holdpaper = true;
+
+            paper.style.zIndex = maxz;
+            maxz = maxz + 1;
+
+            if(e.button == 0){
+                this.premouseX = this.mouseX;
+                this.premouseY = this.mouseY;
+
+                console.log(this.premouseX);
+                console.log(this.premouseY);
+            }
         })
 
         document.addEventListener('mousemove' , (e) => {
-            console.log("Mouse is moving");
+            this.mouseX = e.clientX;
+            this.mouseY = e.clientY;
+
+
+            this.velocityX = this.mouseX - this.premouseX;
+            this.velocityY = this.mouseY - this.premouseY;
+
+            if(this.holdpaper == true){
+                this.currPaperX = this.currPaperX + this.velocityX;
+                this.currPaperY = this.currPaperY + this.velocityY;
+
+
+                this.premouseX = this.mouseX;
+                this.premouseY = this.mouseY;
+
+                paper.style.transform = `translateX(${this.currPaperX}px) translateY(${this.currPaperY}px)`;
+            }
         })
 
         window.addEventListener('mouseup' , (e) => {
