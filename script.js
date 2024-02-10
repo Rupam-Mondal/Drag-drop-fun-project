cc5dd1e1f586b1b487abfe1d559cdc8a614cc6bb
Rupam <rupammondal564@gmail.com>
2024-02-10 18:33:14 +0530

    mouse event added

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-class paper{
+class Paper{
     init(paper){
-        
+        paper.addEventListener('mousedown' , (e) => {
+            console.log("Selected");
+        })
+
+        document.addEventListener('mousemove' , (e) => {
+            console.log("Mouse is moving");
+        })
+
+        window.addEventListener('mouseup' , (e) => {
+            console.log("realeased");
+        })
     }
 }
 
 const papers = Array.from(document.querySelectorAll('.paper'))
 
 papers.forEach(paper => {
-    const p = new paper();
+    const p = new Paper();
     p.init(paper);
 })
